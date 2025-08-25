d684612dca5c7b117b31cffdcf8ffe9497787433
stefan.balea <stefan.balea@axsys.ro>
2025-08-25 19:50:35 +0300

    integrated chain registry

--- src/constants/chain.tsx
+++ src/constants/chain.tsx
@@ -1,20 +1,48 @@
-import { chains, assetLists } from "chain-registry";
+import { chains, assetLists } from "chain-registry/mainnet";
+import { chains as testnetChains, assetLists as testnetAssetLists } from "chain-registry/testnet";
+import { ibcData } from 'chain-registry';
+import {getAssetLists as ibcAssetsList} from "@chain-registry/utils";
 
 export const getChainId = (): string => {
     return process.env.NEXT_PUBLIC_CHAIN_ID || 'beezee-1'
 }
 
+export const getChainName = (): string => {
+    return process.env.NEXT_PUBLIC_CHAIN_NAME || 'beezee'
+}
+
+export const isTestnetChain = (): boolean => {
+    const isTestnet = process.env.NEXT_PUBLIC_CHAIN_IS_TESTNET
+    return isTestnet === 'true' || isTestnet === '1'
+}
+
 export const getWalletChainsNames = () => {
+    let localChains = chains
+    if (isTestnetChain()) {
+        localChains = testnetChains
+    }
+
     const envChainsNames = process.env.NEXT_PUBLIC_WALLET_CHAINS_NAMES
     if (!envChainsNames) {
-        return chains.filter(c => c.chainId === getChainId())
+        return localChains.filter(c => c.chainId === getChainId())
     }
 
     const split = envChainsNames.split(',')
 
-    return chains.filter(c => split.includes(c.chainName.toLowerCase()))
+    return localChains.filter(c => split.includes(c.chainName.toLowerCase()))
 }
 
 export const getAssetLists = () => {
-    return assetLists
+    let localAssetLists = assetLists
+    if (isTestnetChain()) {
+        localAssetLists = testnetAssetLists
+    }
+
+    return localAssetLists
+}
+
+export const getIBCAssetList = () => {
+    const all = ibcAssetsList(getChainName(), ibcData, getAssetLists())
+
+    return all.length > 0 ? all[0].assets : []
 }
--- src/service/assets_factory.tsx
+++ src/service/assets_factory.tsx
@@ -1,9 +1,12 @@
 import {getAllSupply, getAllSupplyMetadata} from "@/query/supply";
 import {MetadataSDKType} from "@bze/bzejs/cosmos/bank/v1beta1/bank";
 import {Asset} from "@/types/asset";
-import {getDenomType, isLpDenom, truncateDenom} from "@/utils/denom";
+import {getDenomType, isIbcDenom, isLpDenom, truncateDenom} from "@/utils/denom";
 import {TOKEN_LOGO_PLACEHOLDER} from "@/constants/placeholders";
-import {STABLE_COINS, VERIFIED_ASSETS} from "@/constants/assets";
+import {EXCLUDED_ASSETS, STABLE_COINS, VERIFIED_ASSETS} from "@/constants/assets";
+import {getIBCAssetList} from "@/constants/chain";
+import {getExponentByDenomFromAsset} from "@chain-registry/utils";
+
 
 // returns all assets from the chain except LP tokens
 export const getChainAssets = async (): Promise<Asset[]> => {
@@ -13,7 +16,7 @@ export const getChainAssets = async (): Promise<Asset[]> => {
     }
 
     return supply
-        .filter(asset => !isLpDenom(asset.denom)) //filter out LP tokens
+        .filter(asset => !isLpDenom(asset.denom) || !EXCLUDED_ASSETS[asset.denom]) //filter out LP tokens & excluded assets
         .map(asset => {
         const metadataEntry = metadata[asset.denom]
 
@@ -33,13 +36,31 @@ const createAsset = (denom: string, meta: MetadataSDKType|undefined): Asset => {
         verified: false
     }
 
+    if (isIbcDenom(asset.denom)) {
+        const ibcList = getIBCAssetList()
+        const ibcData = ibcList.find((item) => item.base === asset.denom)
+
+        if (ibcData) {
+            asset.name = ibcData.name
+            asset.ticker = ibcData.symbol
+            asset.decimals = getExponentByDenomFromAsset(ibcData, asset.denom) ?? 0
+            asset.logo = ibcData.logoURIs?.svg ?? ibcData.logoURIs?.png ?? TOKEN_LOGO_PLACEHOLDER
+        }
+
+        return asset
+    }
+
     if (!meta || meta.base !== denom) {
         return asset;
     }
 
-    asset.name = meta.name
-    asset.ticker = meta.symbol.toUpperCase()
-    //TODO: add logo & override other values from CR
+    if (meta.name.length > 0) {
+        asset.name = meta.name
+    }
+    if (meta.symbol.length > 0) {
+        asset.ticker = meta.symbol.toUpperCase()
+    }
+
     asset.stable = STABLE_COINS[denom] ?? false
     asset.verified = VERIFIED_ASSETS[denom] ?? false
 
