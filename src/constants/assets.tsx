096008ff614645eb801a777f279c99013371828b
stefan.balea <faneatiku@yahoo.com>
2025-12-02 03:05:04 +0200

    updated excluded markets and assets: added new entries to `EXCLUDED_MARKETS` and `EXCLUDED_ASSETS` constants

--- src/constants/assets.tsx
+++ src/constants/assets.tsx
@@ -19,7 +19,8 @@ export const VERIFIED_ASSETS: { [key: string]: boolean } = {
 export const EXCLUDED_ASSETS: { [key: string]: boolean } = {
     "factory/testbz1w9vva0muctcrmd9xgret9x4wasw2rrflsdkwfs/faneatiku1": false,
     "factory/bze1972aqfzdg29ugjln74edx0xvcg4ehvysjptk77/1000000000": true,
-    "ibc/689DD6F80E4DBCE14877462B182504037FAEAD0699D5804A7F5CB328D33ED24B": true
+    "ibc/689DD6F80E4DBCE14877462B182504037FAEAD0699D5804A7F5CB328D33ED24B": true,
+    "factory/bze1f0qgels0eu96ev6a67znu70q7rquy9eragn8nw/ucorey": true,
 }
 
 export const STABLE_COINS: { [key: string]: boolean } = {
