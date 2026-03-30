919eaef6c2fc8381811cd744bf9c5fdb03bf8820
stefan.balea <faneatiku@yahoo.com>
2026-03-30 20:03:27 +0300

    remove custom headers configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,6 @@ const nextConfig: NextConfig = {
         optimizePackageImports: ["@chakra-ui/react", "@bze/bze-ui-kit"]
     },
     transpilePackages: ["@bze/bze-ui-kit"],
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    { key: "X-Frame-Options", value: "ALLOWALL" },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
